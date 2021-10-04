aec56bb2b0a87d28a5cea5929791df819bcdc0c1
MyNameIsLiza <lizatapik@gmail.com>
2021-10-04 12:10:15 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyCEyYh-BEkWFbkqWZUrrN-cJbrqsoSh75c",
+  authDomain: "accident-simulator-liza.firebaseapp.com",
+  databaseURL: "https://accident-simulator-liza-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "accident-simulator-liza",
+  storageBucket: "accident-simulator-liza.appspot.com",
+  messagingSenderId: "200851750236",
+  appId: "1:200851750236:web:8d40639aa8cf0dcecd9599"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+firebase.database().ref('/country').on('value', (elem) => {
+  console.log(elem.val());
+});/**/
 
 function App() {
   return (
